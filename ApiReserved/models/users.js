89cc017a6fc4d7c679889c05e46037e20fc57216
Alejandro Martinez <alejandromartinez1094@gmail.com>
2017-11-09 18:59:53 +0100

    Conexion Node y Mysql y primeros metodos API usuarios

---
+++ ApiReserved/models/users.js
@@ -0,0 +1,105 @@
+var mysql=require("mysql");
+
+/* Conectar con la DB */
+connection=mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "root",
+        database: "reserved"
+});
+
+var User={};
+
+/* Mostar todos los usuarios */
+User.all= function(callback){
+    if (connection){
+        connection.query("SELECT * FROM usuarios",function (error,rows){
+            if (error){
+                throw error;
+            }else{
+                return callback(null,rows);
+            }
+        })
+    }
+}
+
+/* Crear un usuario */
+User.insert=function(userData,callback){
+    if(connection){
+        connection.query("INSERT INTO usuarios SET ?",userData,function(error,result){
+            if (error){
+                throw error;
+            }else{
+                return callback(null,result.insertid);
+            }
+        })
+    }
+}
+
+/* Mostar un usuario buscado por su Id */
+User.findOneById=function(id, callback){
+    if (connection){
+        var sql=("SELECT * FROM usuarios WHERE idUsuario="+connection.escape(id));
+        connection.query(sql,function(error,row){
+            if (error){
+                throw error;
+            }else{
+                return callback(null,row);
+            }
+        })
+    }
+}
+
+/* Modificar un usuario */
+User.update=function(userData,callback) {
+    if(connection){
+        var sql="UPDATE usuarios SET nombre="+connection.escape(userData.nombre)+","+
+                "apellidos="+connection.escape(userData.apellidos)+","+
+                "contraseña="+connection.escape(userData.contraseña)+","+
+                "email="+connection.escape(userData.email)+","+
+                "telefono="+connection.escape(userData.telefono)+","+
+                "direccion="+connection.escape(userData.direccion)+
+                "WHERE idUsuario="+userData.id;
+        connection.query(sql,function(error,result){
+            if(error){
+                throw error;
+            }else{
+                return callback(null,"Usuario actualizado");
+            }
+        })
+    }
+}
+
+/* Eliminar un Usuario */
+User.remove=function(Id,callback){
+    if(connection){
+        var sql= "DELETE FROM usuarios WHERE idUsuario=" + connection.escape(Id);
+        connection.query(sql,function(error,result){
+            if(error){
+                throw error;
+            }else{
+                return callback(null,"Usuario eliminado");
+            }
+        })
+    }
+}
+
+/* PROBAR CONEXION */
+/*
+User.connect=function(callback){
+    if(connection){
+        var sql="SELECT 1+1";
+        connection.query(sql,function(error,row){
+                if(error){
+                    return callback(error,null);
+                }
+                return callback(null,"Conexión establecida con mysql, resultado: "+JSON.stringify(row));
+        })
+    }else{
+        return callback("No se puede conectar",null);
+    }
+}
+*/
+
+
+module.exports=User;
--- ApiReserved/routes/index.js
+++ ApiReserved/routes/index.js
@@ -1,9 +1,98 @@
 var express = require('express');
 var router = express.Router();
+var User=require("../models/users");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET  Todos los Usuarios */
+router.get('/users', function(req, res, next) {
+
+    User.all(function(error,data){
+        if (error){
+            res.json(500,error);
+        }else{
+            res.json(200,data);
+        }
+    })
 });
+
+/* POST Crear un usuario */
+router.post('/users',function(req,res,next){
+    var userData={
+        IdUsuario:null,
+        nick:req.body.nick,
+        contraseña:req.body.contraseña,
+        nombre:req.body.nombre,
+        apellidos:req.body.apellidos,
+        email:req.body.email,
+        telefono:req.body.telefono,
+        direccion:req.body.direccion
+    };
+
+    User.insert(userData,function(error,data){
+        if (error){
+            res.json(500,error);
+        }else{
+            res.json(200,data);
+        }
+    })
+
+});
+
+/* GET Usuario por su Id */
+router.get('/users/:id', function(req, res, next) {
+
+    User.findOneById(req.params.id,function(error,data){
+        if (error){
+            res.json(500,error);
+        }else{
+            res.json(200,data);
+        }
+    })
+});
+
+/* PUT Modificar un usuario */
+router.put('/users/:id',function(req,res,next){
+    var userData={
+        id:req.params.id,
+        contraseña:req.body.contraseña,
+        nombre:req.body.nombre,
+        apellidos:req.body.apellidos,
+        email:req.body.email,
+        telefono:req.body.telefono,
+        direccion:req.body.direccion
+    };
+
+    User.update(userData,function(error,data){
+        if (error){
+            res.json(500,error);
+        }else{
+            res.json(200,data);
+        }
+    })
+
+});
+
+/* DELETE Borrar un usuario */
+router.delete("/users/:id",function(req,res,next){
+    User.remove(req.params.id,function(error,data){
+        if (error){
+            res.json(500,error);
+        }else{
+            res.json(200,data);
+        }
+    })
+});
+
+/* Prueba CONEXION */
+/*
+router.get('/users', function(req, res, next) {
+  User.connect(function(error,res){
+      if(error){
+          console.log(JSON.stringify(error));
+      }else{
+          console.log(res);
+      }
+  })
+});
+*/
 
 module.exports = router;
