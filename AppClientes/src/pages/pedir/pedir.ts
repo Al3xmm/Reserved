39ce59eed2d9cf15273b3e7309245c2547bca54e
SergioLazaroFrias <serlazf@hotmail.com>
2018-04-21 00:23:30 +0200

    Maquetacion alergenos y copiarlos en pedir

--- AppClientes/src/pages/pedir/pedir.ts
+++ AppClientes/src/pages/pedir/pedir.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams,ModalController, LoadingController } from 'ionic-angular';
 import { RestaurantsProvider } from './../../providers/restaurants/restaurants';
 import { PedidoProvider } from './../../providers/pedido/pedido';
 import { MipedidoPage } from './../mipedido/mipedido';
@@ -18,7 +18,7 @@ import { MipedidoPage } from './../mipedido/mipedido';
 })
 export class PedirPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public restaurantService: RestaurantsProvider, public pedidoService: PedidoProvider ) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public restaurantService: RestaurantsProvider, public pedidoService: PedidoProvider,private modalCtrl:ModalController, private loadingCtrl:LoadingController ) {
   }
 
   ionViewDidLoad() {
@@ -27,5 +27,23 @@ export class PedirPage {
 
   verpedido(){
     this.navCtrl.push(MipedidoPage);
+  }
+  mas_info(){
+    let modal = this.modalCtrl.create('InfoproductoPage');
+    modal.present();
+  }
+
+  presentLoadingCustom(idproducto) {
+    this.restaurantService.masinfoproducto(idproducto);
+    let loading = this.loadingCtrl.create({
+      content: 'Cargando...',
+      duration: 1000,
+    });
+
+    loading.onDidDismiss(() => {
+      this.mas_info();
+    });
+
+    loading.present();
   }
 }
